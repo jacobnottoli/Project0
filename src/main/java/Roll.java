53e27050459e0329f3aaa5f2f46603f9150c1440
Jacob Nottoli <jacob.nottoli@gmail.com>
2022-08-29 14:03:31 -0500

    added roll class

---
+++ src/main/java/Roll.java
@@ -0,0 +1,28 @@
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Roll {
+
+    public static int d20() {
+        return ThreadLocalRandom.current().nextInt(1,21);
+    }
+
+    public static int d12() {
+        return ThreadLocalRandom.current().nextInt(1,13);
+    }
+
+    public static int d10() {
+        return ThreadLocalRandom.current().nextInt(1,11);
+    }
+
+    public static int d8() {
+        return ThreadLocalRandom.current().nextInt(1,9);
+    }
+
+    public static int d6() {
+        return ThreadLocalRandom.current().nextInt(1,7);
+    }
+
+    public static int d4() {
+        return ThreadLocalRandom.current().nextInt(1,5);
+    }
+}
