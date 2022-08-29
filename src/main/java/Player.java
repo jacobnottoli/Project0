ab9ff26f2ba16a4a199ad6d8aa6189c106ad1b67
Jacob Nottoli <jacob.nottoli@gmail.com>
2022-08-29 13:49:54 -0500

    First commit

---
+++ src/main/java/Player.java
@@ -0,0 +1,39 @@
+public class Player {
+    String name;
+    int health;
+    int ac;
+    String clas;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getHealth() {
+        return this.health;
+    }
+
+    public void setAC(int ac) {
+        this.ac = ac;
+    }
+
+    public int getAC() {
+        return this.ac;
+    }
+
+    public void setClas(String clas) {
+        this.clas = clas;
+    }
+
+    public String getClas() {
+        return this.clas;
+    }
+}
+
