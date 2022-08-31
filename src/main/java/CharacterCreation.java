18b76dd876bbe8c713ff27cfeb64f99ca2777a1f
Jacob Nottoli <jacob.nottoli@gmail.com>
2022-08-31 12:37:24 -0500

    Started JDBC, Completed CharacterCreation *avoids bad user input

--- src/main/java/CharacterCreation.java
+++ src/main/java/CharacterCreation.java
@@ -26,81 +26,138 @@ public class CharacterCreation {
     public static void createMage(Player p){
         System.out.println("Ahhh... An adventurer of the arcane!\nWhat type of mage are you?");
         System.out.println("1. Fire\n2. Frost");
-        if (in.nextInt()==1) {
+        int pick = in.nextInt();
+        if (pick==1) {
             System.out.println("Well! A fire mage sure does know how to do some damage! It looks like you could be the right person for the job!");
             p.setClas("Fire Mage");
             p.setDMG(12);
             p.setHeal(6);
-            System.out.println("\nLet's roll for the first time! Let's see how much health you have.");
-            System.out.println("\n1. Roll for health!");
-            if (in.nextInt()==1) {
-                p.setHealth(20+Roll.d12());
-            }
+            CharacterCreation.hpFiM(p);
             System.out.println("You have " + p.getHealth() + " hit points!");
-            System.out.println("\nLet's roll for the second time! Let's see what your armor class is!");
-            System.out.println("\n1. Roll for AC!");
-            if (in.nextInt()==1) {
-                p.setAC(10+Roll.d4());
-            }
+            CharacterCreation.acFiM(p);
             System.out.println("You have an armor class of " + p.getAC() + "!");
-        } else {
+        } else if (pick==2){
             System.out.println("Well! Frost mages are very well rounded! It looks like you could be the right person for the job!");
             p.setClas("Frost Mage");
             p.setDMG(10);
             p.setHeal(8);
-            System.out.println("\nLet's roll for the first time! Let's see how much health you have.");
-            System.out.println("\n1. Roll for health!");
-            if (in.nextInt()==1) {
-                p.setHealth(20+Roll.d12());
-            }
+            CharacterCreation.hpFrM(p);
             System.out.println("You have " + p.getHealth() + " hit points!");
-            System.out.println("\nLet's roll for the second time! Let's see what your armor class is!");
-            System.out.println("\n1. Roll for AC!");
-            if (in.nextInt()==1) {
-                p.setAC(10+Roll.d4());
-            }
+            CharacterCreation.acFrM(p);
             System.out.println("You have an armor class of " + p.getAC() + "!");
 
+        } else {
+            CharacterCreation.createMage(p);
         }
     }
 
     public static void createWarrior(Player p) {
         System.out.println("Ahhh... An adventurer of the brawn!\nWhat type of warrior are you?");
         System.out.println("1. One Handed\n2. Two Handed");
-        if (in.nextInt()==1) {
+        int pick = in.nextInt();
+        if (pick==1) {
             System.out.println("Well! One handed warriors know how to protect themselves with their shield! It looks like you could be the right person for the job!");
             p.setClas("One Handed Warrior");
             p.setDMG(8);
             p.setHeal(4);
-            System.out.println("\nLet's roll for the first time! Let's see how much health you have.");
-            System.out.println("\n1. Roll for health!");
-            if (in.nextInt()==1) {
-                p.setHealth(30+Roll.d6());
-            }
+            CharacterCreation.hpOW(p);
             System.out.println("You have " + p.getHealth() + " hit points!");
-            System.out.println("\nLet's roll for the second time! Let's see what your armor class is!");
-            System.out.println("\n1. Roll for AC!");
-            if (in.nextInt()==1) {
-                p.setAC(12+Roll.d6());
-            }
+            CharacterCreation.acOW(p);
             System.out.println("You have an armor class of " + p.getAC() + "!");
-        } else {
+        } else if (pick==2){
             System.out.println("Well! Two handed warriors are excellent! It looks like you could be the right person for the job!");
             p.setClas("Two Handed Warrior");
             p.setDMG(10);
             p.setHeal(4);
-            System.out.println("\nLet's roll for the first time! Let's see how much health you have.");
-            System.out.println("\n1. Roll for health!");
-            if (in.nextInt()==1) {
-                p.setHealth(30+Roll.d6());
-            }
+            CharacterCreation.hpTW(p);
             System.out.println("You have " + p.getHealth() + " hit points!");
-            System.out.println("\nLet's roll for the second time! Let's see what your armor class is!");
-            System.out.println("\n1. Roll for AC!");
-            if (in.nextInt()==1) {
-                p.setAC(10+Roll.d6());
-            }
+            CharacterCreation.acTW(p);
             System.out.println("You have an armor class of " + p.getAC() + "!");
+        } else {
+            System.out.println("\nInvalid Input!");
+            CharacterCreation.createWarrior(p);
+        }
+    }
+
+    public static void printRollHealth() {
+        System.out.println("\nLet's roll for the first time! Let's see how much health you have.");
+        System.out.println("\n1. Roll for health!");
+    }
+
+    public static void printRollAC() {
+        System.out.println("\nLet's roll for the second time! Let's see what your armor class is!");
+        System.out.println("\n1. Roll for armor class!");
+    }
+    public static void hpFiM(Player p) {
+        CharacterCreation.printRollHealth();
+        if (in.nextInt()==1) {
+            p.setHealth(20+Roll.d12());
+        } else {
+            System.out.println("\nInvalid Input! Press 1 to roll");
+            CharacterCreation.hpFiM(p);
+        }
+    }
+    public static void hpFrM(Player p) {
+        CharacterCreation.printRollHealth();
+        if (in.nextInt()==1) {
+            p.setHealth(20+Roll.d12());
+        } else {
+            System.out.println("\nInvalid Input! Press 1 to roll.");
+            CharacterCreation.hpFrM(p);
+        }
+    }
+    public static void hpOW(Player p) {
+        CharacterCreation.printRollHealth();
+        if (in.nextInt()==1) {
+            p.setHealth(30+Roll.d6());
+        } else {
+            System.out.println("\nInvalid Input! Press 1 to roll.");
+            CharacterCreation.hpOW(p);
+        }
+    }
+    public static void hpTW(Player p) {
+        CharacterCreation.printRollHealth();
+        if (in.nextInt()==1) {
+            p.setHealth(30+Roll.d6());
+        } else {
+            System.out.println("\nInvalid Input! Press 1 to roll.");
+            CharacterCreation.hpTW(p);
+        }
+    }
+    public static void acFiM(Player p) {
+        CharacterCreation.printRollAC();
+        if (in.nextInt()==1) {
+            p.setAC(10+Roll.d4());
+        } else {
+            System.out.println("\nInvalid Input! Press 1 to roll.");
+            CharacterCreation.acFiM(p);
+        }
+    }
+    public static void acFrM(Player p) {
+        CharacterCreation.printRollAC();
+        if (in.nextInt()==1) {
+            p.setAC(10+Roll.d4());
+        } else {
+            System.out.println("\nInvalid Input! Press 1 to roll.");
+            CharacterCreation.acFrM(p);
+        }
+    }
+    public static void acOW(Player p) {
+        CharacterCreation.printRollAC();
+        if (in.nextInt()==1) {
+            p.setAC(12+Roll.d6());
+        } else {
+            System.out.println("\nInvalid Input! Press 1 to roll.");
+            CharacterCreation.acOW(p);
+        }
+    }
+    public static void acTW(Player p) {
+        CharacterCreation.printRollAC();
+        if (in.nextInt()==1) {
+            p.setAC(10+Roll.d6());
+        } else {
+            System.out.println("\nInvalid Input! Press 1 to roll.");
+            CharacterCreation.acTW(p);
         }
     }
 }
--- src/main/java/FirstEncounter.java
+++ src/main/java/FirstEncounter.java
@@ -4,7 +4,7 @@ public class FirstEncounter {
     public static void Encounter(Player p) {
         Scanner in = new Scanner(System.in);
         System.out.println("\nOur journey begins on the road that leads west out of the city of Colfax...Queen Elaine warned of troubles...Let's see what type of troubles you encounter first...\n1. Roll for your encounter");
-        int roll = in.nextInt();
+        int roll = Roll.d20();
         System.out.println("You rolled a: " + roll +"!");
     }
 
